fc0346db641ceb261a7e60cbb14a445a8e04bb94
Andres <extern.andres.singh@vwfs.io>
2023-03-29 13:31:47 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import { ethers } from "./ethers-5.6.esm.min.js";
+import { abi, contractAddress } from "./constants.js";
+
+const connectButton = document.getElementById("connectButton");
+
+const fundButton = document.getElementById("fundButton");
+
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+
+async function connect() {
+	if (typeof window.ethereum !== "undefined") {
+		try {
+			await window.ethereum.request({ method: "eth_requestAccounts" });
+		} catch (error) {
+			console.log(error);
+		}
+		connectButton.innerHTML = "Connected!";
+	} else {
+		connectButton.innerHTML = "Please install Metamask!";
+	}
+}
+
+async function fund() {
+	const ethAmount = "3";
+	if (typeof window.ethereum !== "undefined") {
+		const provider = new ethers.providers.Web3Provider(window.ethereum);
+		const signer = provider.getSigner();
+		const contract = new ethers.Contract(contractAddress, abi, signer);
+		try {
+			const transactionResponse = await contract.fund({
+				value: ethers.utils.parseEther(ethAmount),
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}
+}
